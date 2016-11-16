709e87bf69aee150982477c8c8d7d084b75d115f
Adam Wathan <adam.wathan@gmail.com>
2016-11-16 16:37:49 -0500

    4.1 - Asserting Against JSON Responses

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -41,6 +41,13 @@ class PurchaseTicketsTest extends TestCase
         ]);
 
         $this->assertResponseStatus(201);
+
+        $this->seeJsonSubset([
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'amount' => 9750,
+        ]);
+
         $this->assertEquals(9750, $this->paymentGateway->totalCharges());
         $this->assertTrue($concert->hasOrderFor('john@example.com'));
         $this->assertEquals(3, $concert->ordersFor('john@example.com')->first()->ticketQuantity());
