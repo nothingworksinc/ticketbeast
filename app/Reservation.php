990f873e0c95d6010af1a7661a551c3ebbeb74e3
Adam Wathan <adam.wathan@gmail.com>
2016-12-16 08:31:01 -0500

    52 - Avoiding Service Classes with Method Injection

--- app/Reservation.php
+++ app/Reservation.php
@@ -28,8 +28,10 @@ class Reservation
         return $this->email;
     }
 
-    public function complete()
+    public function complete($paymentGateway, $paymentToken)
     {
+        $paymentGateway->charge($this->totalCost(), $paymentToken);
+
         return Order::forTickets($this->tickets(), $this->email(), $this->totalCost());
     }
 
