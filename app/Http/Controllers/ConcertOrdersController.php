43270f59d23b31aa0b1f06c67d525b9b53620d59
Adam Wathan <adam.wathan@gmail.com>
2016-12-16 08:31:01 -0500

    52 - Avoiding Service Classes with Method Injection

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -30,17 +30,9 @@ class ConcertOrdersController extends Controller
         ]);
 
         try {
-            // Find some tickets
             $reservation = $concert->reserveTickets(request('ticket_quantity'), request('email'));
-
-            // Charge the customer for the tickets
-            $this->paymentGateway->charge($reservation->totalCost(), request('payment_token'));
-
-            // Create an order for those tickets
-            $order = $reservation->complete();
-
+            $order = $reservation->complete($this->paymentGateway, request('payment_token'));
             return response()->json($order, 201);
-
         } catch (PaymentFailedException $e) {
             $reservation->cancel();
             return response()->json([], 422);
--- tests/unit/ReservationTest.php
+++ tests/unit/ReservationTest.php
@@ -3,6 +3,7 @@
 use App\Ticket;
 use App\Concert;
 use App\Reservation;
+use App\Billing\FakePaymentGateway;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -71,11 +72,13 @@ class ReservationTest extends TestCase
         $concert = factory(Concert::class)->create(['ticket_price' => 1200]);
         $tickets = factory(Ticket::class, 3)->create(['concert_id' => $concert->id]);
         $reservation = new Reservation($tickets, 'john@example.com');
+        $paymentGateway = new FakePaymentGateway;
 
-        $order = $reservation->complete();
+        $order = $reservation->complete($paymentGateway, $paymentGateway->getValidTestToken());
 
         $this->assertEquals('john@example.com', $order->email);
         $this->assertEquals(3, $order->ticketQuantity());
         $this->assertEquals(3600, $order->amount);
+        $this->assertEquals(3600, $paymentGateway->totalCharges());
     }
 }
