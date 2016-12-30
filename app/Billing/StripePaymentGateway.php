2d4ce1dc9ea4db554f0cff3312cb72da17ce9826
Adam Wathan <adam.wathan@gmail.com>
2016-12-30 15:18:39 -0500

    (remove Stripe mocking examples)

--- app/Billing/StripePaymentGateway.php
+++ app/Billing/StripePaymentGateway.php
@@ -22,27 +22,3 @@ class StripePaymentGateway implements PaymentGateway
         ], ['api_key' => $this->apiKey]);
     }
 }
-
-// class StripePaymentGateway implements PaymentGateway
-// {
-//     private $apiKey;
-
-//     public function __construct($apiKey)
-//     {
-//         $this->apiKey = $apiKey;
-//     }
-
-//     public function charge($amount, $token)
-//     {
-//         (new \GuzzleHttp\Client)->post('https://api.stripe.com/v1/charges', [
-//             'headers' => [
-//                 'Authorization' => "Bearer {$this->apiKey}",
-//             ],
-//             'form_params' => [
-//                 'amount' => $amount,
-//                 'token' => $token,
-//                 'currency' => 'usd',
-//             ]
-//         ]);
-//     }
-// }
