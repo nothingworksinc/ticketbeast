16a4234d0f57e49f62a728e2f806936536245a8a
Adam Wathan <adam.wathan@gmail.com>
2017-03-13 12:40:18 -0400

    78 - Updating Our Unit Tests

--- app/Order.php
+++ app/Order.php
@@ -46,6 +46,7 @@ class Order extends Model
     public function toArray()
     {
         return [
+            'confirmation_number' => $this->confirmation_number,
             'email' => $this->email,
             'ticket_quantity' => $this->ticketQuantity(),
             'amount' => $this->amount,
--- tests/unit/OrderTest.php
+++ tests/unit/OrderTest.php
@@ -54,12 +54,17 @@ class OrderTest extends TestCase
     /** @test */
     function converting_to_an_array()
     {
-        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(5);
-        $order = $concert->orderTickets('jane@example.com', 5);
+        $order = factory(Order::class)->create([
+            'confirmation_number' => 'ORDERCONFIRMATION1234',
+            'email' => 'jane@example.com',
+            'amount' => 6000,
+        ]);
+        $order->tickets()->saveMany(factory(Ticket::class)->times(5)->create());
 
         $result = $order->toArray();
 
         $this->assertEquals([
+            'confirmation_number' => 'ORDERCONFIRMATION1234',
             'email' => 'jane@example.com',
             'ticket_quantity' => 5,
             'amount' => 6000,
