4c92118426e51aad1871ae345a11e88681815a86
Adam Wathan <adam.wathan@gmail.com>
2017-04-01 14:26:33 -0400

    94 - Ready to Demo

--- resources/assets/js/components/TicketCheckout.vue
+++ resources/assets/js/components/TicketCheckout.vue
@@ -95,7 +95,7 @@
                     ticket_quantity: this.quantity,
                     payment_token: token.id,
                 }).then(response => {
-                    console.log("Charge succeeded")
+                    window.location = `/orders/${response.data.confirmation_number}`
                 }).catch(response => {
                     this.processing = false
                 })
