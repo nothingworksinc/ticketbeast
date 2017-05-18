1ab6082926b1d7f5e77b153aa0c5346a72fc1519
Adam Wathan <adam.wathan@gmail.com>
2017-05-18 12:07:14 -0400

    (add empty AddConcertTest)

---
+++ tests/Feature/Backstage/AddConcertTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature\Backstage;
+
+use App\User;
+use App\Concert;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class AddConcertTest extends TestCase
+{
+    use DatabaseMigrations;
+}
