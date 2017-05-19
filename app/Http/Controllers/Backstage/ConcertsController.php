9b48c7f78e391112063aa60222ef3ccd4dcf9709
Adam Wathan <adam.wathan@gmail.com>
2017-05-19 13:59:41 -0400

    104 - Adding a Valid Concert

--- app/Http/Controllers/Backstage/ConcertsController.php
+++ app/Http/Controllers/Backstage/ConcertsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Backstage;
 
+use App\Concert;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,5 +12,26 @@ class ConcertsController extends Controller
     public function create()
     {
         return view('backstage.concerts.create');
+    }
+
+    public function store()
+    {
+        $concert = Concert::create([
+            'title' => request('title'),
+            'subtitle' => request('subtitle'),
+            'date' => Carbon::parse(vsprintf('%s %s', [
+                request('date'),
+                request('time'),
+            ])),
+            'ticket_price' => request('ticket_price') * 100,
+            'venue' => request('venue'),
+            'venue_address' => request('venue_address'),
+            'city' => request('city'),
+            'state' => request('state'),
+            'zip' => request('zip'),
+            'additional_information' => request('additional_information'),
+        ])->addTickets(request('ticket_quantity'));
+
+        return redirect()->route('concerts.show', $concert);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/concerts/{id}', 'ConcertsController@show');
+Route::get('/concerts/{id}', 'ConcertsController@show')->name('concerts.show');
 Route::post('/concerts/{id}/orders', 'ConcertOrdersController@store');
 Route::get('/orders/{confirmationNumber}', 'OrdersController@show');
 
@@ -19,6 +19,8 @@ Route::get('/login', 'Auth\LoginController@showLoginForm')->name('auth.show-logi
 Route::post('/login', 'Auth\LoginController@login')->name('auth.login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('auth.logout');
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/backstage/concerts/new', 'Backstage\ConcertsController@create');
+Route::group(['middleware' => 'auth', 'prefix' => 'backstage', 'namespace' => 'Backstage'], function () {
+    Route::get('/concerts/new', 'ConcertsController@create');
+    Route::post('/concerts', 'ConcertsController@store');
 });
+
