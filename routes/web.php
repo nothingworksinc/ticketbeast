9fcd1a7143021ae6713f5e345e34047711ae31f5
Adam Wathan <adam.wathan@gmail.com>
2017-05-19 13:38:44 -0400

    103 - Preventing Guests from Adding New Concerts

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/login', 'Auth\LoginController@showLoginForm')->name('auth.show-logi
 Route::post('/login', 'Auth\LoginController@login')->name('auth.login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('auth.logout');
 
-Route::get('/backstage/concerts/new', 'Backstage\ConcertsController@create');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/backstage/concerts/new', 'Backstage\ConcertsController@create');
+});
--- tests/Feature/Backstage/AddConcertTest.php
+++ tests/Feature/Backstage/AddConcertTest.php
@@ -11,4 +11,23 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class AddConcertTest extends TestCase
 {
     use DatabaseMigrations;
+
+    /** @test */
+    function promoters_can_view_the_add_concert_form()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get('/backstage/concerts/new');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    function guests_cannot_view_the_add_concert_form()
+    {
+        $response = $this->get('/backstage/concerts/new');
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
 }
