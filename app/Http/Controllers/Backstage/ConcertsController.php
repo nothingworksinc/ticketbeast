d192bba9cecbbff93f37b9f2bb9bcd1e1839494c
Adam Wathan <adam.wathan@gmail.com>
2017-06-06 19:19:07 -0400

    111 - Avoiding Sort-Sensitive Tests

--- app/Http/Controllers/Backstage/ConcertsController.php
+++ app/Http/Controllers/Backstage/ConcertsController.php
@@ -12,7 +12,7 @@ class ConcertsController extends Controller
 {
     public function index()
     {
-        return view('backstage.concerts.index', ['concerts' => Concert::all()]);
+        return view('backstage.concerts.index', ['concerts' => Auth::user()->concerts]);
     }
 
     public function create()
--- tests/Feature/Backstage/ViewConcertListTest.php
+++ tests/Feature/Backstage/ViewConcertListTest.php
@@ -30,13 +30,18 @@ class ViewConcertListTest extends TestCase
     {
         $this->disableExceptionHandling();
         $user = factory(User::class)->create();
-        $concerts = factory(Concert::class, 3)->create(['user_id' => $user->id]);
+        $otherUser = factory(User::class)->create();
+        $concertA = factory(Concert::class)->create(['user_id' => $user->id]);
+        $concertB = factory(Concert::class)->create(['user_id' => $user->id]);
+        $concertC = factory(Concert::class)->create(['user_id' => $otherUser->id]);
+        $concertD = factory(Concert::class)->create(['user_id' => $user->id]);
 
         $response = $this->actingAs($user)->get('/backstage/concerts');
 
         $response->assertStatus(200);
-        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[0]));
-        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[1]));
-        $this->assertTrue($response->original->getData()['concerts']->contains($concerts[2]));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concertA));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concertB));
+        $this->assertTrue($response->original->getData()['concerts']->contains($concertD));
+        $this->assertFalse($response->original->getData()['concerts']->contains($concertC));
     }
 }
