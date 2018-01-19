ec1986eb8a0fe2572c8be0b61791d8bda1e3299f
Adam Wathan <adam.wathan@gmail.com>
2018-01-19 10:44:00 -0500

    156 - Getting Cozy with Stripe Connect

---
+++ tests/Browser/ConnectWithStripeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ConnectWithStripeTest extends DuskTestCase
+{
+    /** @test */
+    public function connecting_a_stripe_account_successfully()
+    {
+        $user = factory(User::class)->create([
+            'stripe_account_id' => null,
+            'stripe_access_token' => null,
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                    ->visit('/backstage/stripe-connect/authorize')
+                    ->assertUrlIs('https://connect.stripe.com/oauth/authorize')
+                    ->assertQueryStringHas('response_type', 'code')
+                    ->assertQueryStringHas('scope', 'read_write')
+                    ->assertQueryStringHas('client_id', config('services.stripe.client_id'))
+                    ;
+        });
+    }
+}
