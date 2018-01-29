54a8159a1d57b9a0c2cd26ea0db96192db1f04a4
Adam Wathan <adam.wathan@gmail.com>
2018-01-29 13:13:16 -0500

    162 - Updating Factories and a Speed Trick

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,8 +19,10 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
     return [
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => '$2y$10$OExhSFi9YnXROYfb6TQqAeru74fJ0NDVrDQ5cdKi/3TNpxqm50MMu', // "secret"
         'remember_token' => str_random(10),
+        'stripe_account_id' => 'test_acct_1234',
+        'stripe_access_token' => 'test_token',
     ];
 });
 
